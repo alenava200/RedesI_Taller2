9ac209ca46bf6670e045fa7c74b1d893c3ab0e8d
alenava200 <33487082+alenava200@users.noreply.github.com>
2017-11-19 12:37:43 -0400

    Cliente y Servidor con los 5 minutos

--- svr_c.c
+++ svr_c.c
@@ -1,124 +1,309 @@
-#include <stdio.h>
-#include <string.h>    
-#include <stdlib.h>    
-#include <sys/socket.h>
+#include <stdio.h> 
+#include <string.h>
+#include <sys/socket.h> 
 #include <arpa/inet.h> 
-#include <unistd.h>    
-#include <pthread.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <pthread.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <errno.h>
 
-typedef struct mensaje {
-        int serial;
-        char fecha[10];
-        char hora[6];
-        int ID;
-        int codigo;
-        char patron[1000];
-        char info[1000];
-    } MSG;
- 
-//Función de los Hilos
-void *connection_handler(void *);
- 
+#include "header.h"
+
+int input_timeout (int filedes, unsigned int seconds){		// Se crea una función que recibe el file descriptor del socket y una cantidad 
+															// de tiempo en segundos	
+	fd_set set;												
+	struct timeval timeout;
+
+
+	FD_ZERO (&set);											// Se inicializa el fd del socket
+	FD_SET (filedes, &set);
+
+	timeout.tv_sec = seconds;								// Inicializa la estructura de tiempo
+	timeout.tv_usec = 0;
+
+	return (select (FD_SETSIZE,&set, NULL, NULL,&timeout)); // Devuelve 1 si está activo el canal; 0 si no está activo y -1 en caso de error
+}
+
+
+
+
 int main(int argc , char *argv[])
 {
-    int socket_desc , client_sock , c;
-    struct sockaddr_in server , client;
-     
-    //Create socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-     
-    //Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
-     
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        //print the error message
-        perror("bind failed. Error");
-        return 1;
-    }
-    puts("bind done");
-     
-    //Listen
-    listen(socket_desc , 3);
-     
-    //Accept and incoming connection
-    puts("Esperando Conexion...");
-    c = sizeof(struct sockaddr_in);
-     
-     
-    //Accept and incoming connection
-    puts("Esperando Conexion...");
-    c = sizeof(struct sockaddr_in);
-	pthread_t thread_id;
-	
-    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-    {
-        puts("Conexion Aceptada");
-         
-        if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &client_sock) < 0)
-        {
-            perror("No se pudo crear el hilo");
-            return 1;
-        }
-         
-        
-        puts("Handler assigned");
-    }
-     
-    if (client_sock < 0)
-    {
-        perror("accept failed");
-        return 1;
-    }
-     
+    int sock, port, local_port;								// sock: file descriptor del socket. port: puerto del servidor. local_port: puerto del cliente.
+    struct sockaddr_in server;								// server almacenara los parametros del servidor
+    struct sockaddr_in client;								// Estructura usada para almacenar el puerto del cliente
+    char *message = (char*) malloc(2048*sizeof(char)); 		// Cadena de caracteres que almacena los mensajes que se transmitiran al servidor
+    char *ip_srv = (char *) malloc(512*sizeof(char)); 		// Cadena de caracteres donde se almacena la direccion ip del servidor indicado por el usuario
+    int flag = 0, flag2=0;			 						// flag: indica cuando el servidor esta caido. flag2: 1 cuando se indica el servidor por nombre de dominio
+    int flag3 = 0;											// flag3: indica cuando se pasa el puerto del cliente como parametro
+    struct hostent *servidor; 								// Estructura unicamente usada si se indica el servidor por nombre de dominio
+
+    // Cadenas de caracteres auxiliares que son usadas cuando se cae la conexion con el servidor para encolar los mensajes que se tratan de enviar sin conexion
+    char *pp = (char *) malloc(2048*sizeof(char));
+    char *sp = (char *) malloc(2048*sizeof(char));
+
+    // Selector que permite que el programa funcione con la sintaxis deseada svr_c -d <nombre_módulo_central> -p <puerto_svr_s> [-l <puerto_local>]
+    switch(argc) 									
+	{
+		case 5:	
+				if ((strcmp(argv[1],"-d") == 0) && (strcmp(argv[3],"-p") == 0))
+			    {
+			    	strcpy(ip_srv, argv[2]);
+			    	
+			    	if (ip_srv[0] < 48 || ip_srv[0] > 57) 						// En caso de que se indique el servidor por nombre de dominio
+			    	{
+			    		servidor = gethostbyname(argv[2]); 
+			    		if (!servidor )
+			    		{
+			    			puts("Invalid server addres");
+			    			return -1;
+			    		}
+			       		flag2 = 1;
+			       	}
+			    	
+			    	port = atoi(argv[4]);
+			    }
+			    else if((strcmp(argv[1],"-p") == 0) && (strcmp(argv[3],"-d") == 0))
+			    {
+			    	strcpy(ip_srv, argv[4]); 
+
+			    	if (ip_srv[0] < 48 || ip_srv[0] > 57) 						// En caso de que se indique el servidor por nombre de dominio
+			    	{
+			    		servidor = gethostbyname(argv[4]);
+			    		if (!servidor )
+			    		{
+			    			puts("Invalid server addres");
+			    			return -1;
+			    		}
+			       		flag2 = 1;
+			        }
+			    	
+			    	port = atoi(argv[2]);
+			    }
+			    break;
+
+		case 7:	
+			    if ((strcmp(argv[1],"-d") == 0) && (strcmp(argv[3],"-p") == 0) && (strcmp(argv[5],"-l") == 0))
+			    {
+			    	strcpy(ip_srv, argv[2]);
+
+			    	if (ip_srv[0] < 48 || ip_srv[0] > 57) 			// En caso de que se indique el servidor por nombre de dominio
+			    	{
+			    		servidor = gethostbyname(argv[2]); 
+			       		if (!servidor )
+			    		{
+			    			puts("Invalid server addres");
+			    			return -1;
+			    		}
+			       		flag2 = 1;
+			        }
+			    	
+			    	port = atoi(argv[4]);
+			    	local_port = atoi(argv[6]);
+			    }
+			    else if((strcmp(argv[1],"-d") == 0) && (strcmp(argv[3],"-l") == 0) && (strcmp(argv[5],"-p") == 0))
+			    {
+			    	strcpy(ip_srv, argv[2]);
+
+			    	if (ip_srv[0] < 48 || ip_srv[0] > 57) 		// En caso de que se indique el servidor por nombre de dominio
+			    	{
+			    		servidor = gethostbyname(argv[2]); 
+			       		if (!servidor )
+			    		{
+			    			puts("Invalid server addres");
+			    			return -1;
+			    		}
+			       		flag2 = 1;
+			        } 
+
+			    	port = atoi(argv[6]);
+			    	local_port = atoi(argv[4]);
+			    }
+			    else if((strcmp(argv[1],"-p") == 0) && (strcmp(argv[3],"-d") == 0) && (strcmp(argv[5],"-l") == 0))
+			    {
+			    	strcpy(ip_srv, argv[4]); 
+
+			    	if (ip_srv[0] < 48 || ip_srv[0] > 57)  		// En caso de que se indique el servidor por nombre de dominio
+			    	{
+			    		servidor = gethostbyname(argv[4]); 
+			       		if (!servidor )
+			    		{
+			    			puts("Invalid server addres");
+			    			return -1;
+			    		}
+			       		flag2 = 1;
+			        }
+			    	
+			    	port = atoi(argv[2]);
+			    	local_port = atoi(argv[6]);
+			    }
+			    else if((strcmp(argv[1],"-p") == 0) && (strcmp(argv[3],"-l") == 0) && (strcmp(argv[5],"-d") == 0))
+			    {
+			    	strcpy(ip_srv, argv[6]);
+
+			    	if (ip_srv[0] < 48 || ip_srv[0] > 57) 			// En caso de que se indique el servidor por nombre de dominio
+			    	{
+			    		servidor = gethostbyname(argv[6]); 
+			    		if (!servidor )
+			    		{
+			    			puts("Invalid server addres");
+			    			return -1;
+			    		}
+			    		flag2 = 1;
+			    	}			    		  
+			    	
+			    	port = atoi(argv[2]);
+			    	local_port = atoi(argv[4]);
+			    }
+			    else if((strcmp(argv[1],"-l") == 0) && (strcmp(argv[3],"-d") == 0) && (strcmp(argv[5],"-p") == 0))
+			    {
+			    	strcpy(ip_srv, argv[4]);
+
+			    	if (ip_srv[0] < 48 || ip_srv[0] > 57) 			// En caso de que se indique el servidor por nombre de dominio
+			    	{
+			    		servidor = gethostbyname(argv[4]);
+			    		if (!servidor )
+			    		{
+			    			puts("Invalid server addres");
+			    			return -1;
+			    		}
+			    		flag2 = 1;
+			    	} 
+			    	
+			    	port = atoi(argv[6]);
+			    	local_port = atoi(argv[2]);
+			    }
+			    else if((strcmp(argv[1],"-l") == 0) && (strcmp(argv[3],"-p") == 0) && (strcmp(argv[5],"-d") == 0))
+			    {
+			    	strcpy(ip_srv, argv[6]);
+			    	
+			    	if (ip_srv[0] < 48 || ip_srv[0] > 57)  				// En caso de que se indique el servidor por nombre de dominio
+			    	{	
+			    		servidor = gethostbyname(argv[6]); 
+			    		if (!servidor )
+			    		{
+			    			puts("Invalid server addres");
+			    			return -1;
+			    		}
+			    		flag2 = 1;
+			    	}
+			    		  
+			    	port = atoi(argv[4]);
+			    	local_port = atoi(argv[2]);
+			    }
+			    flag3=1;
+				break;
+
+		default:
+				printf("Wrong Sintaxis.\nsvr_c -d <nombre_módulo_central> -p <puerto_svr_s> [-l <puerto_local>]\n");
+    			exit(-1);
+	}
+	while(1)
+	{
+	    //Create socket
+	    sock = socket(AF_INET , SOCK_STREAM , 0);
+
+	    if (sock == -1)
+	    {
+	        printf("Could not create socket");
+	        continue;
+	    }
+
+	    if (flag2) 			// Si el servidor se indico por nombre de dominio 
+	    {
+	    	bzero((char *)&server, sizeof((char *)&server));
+	    	bcopy((char *)servidor->h_addr, (char *)&server.sin_addr.s_addr, sizeof(servidor->h_length));
+	    }
+	    else
+	       	server.sin_addr.s_addr = inet_addr(ip_srv); 			// Se indica la direccion del servidor indicado por usuario
+	    
+	    server.sin_family = AF_INET;							// IPv4
+	    server.sin_port = htons(port);							// Se indica el puerto por donde escucha el servidor indicado por el usario
+	 
+	 	if (flag3) 													//Si el usuario indico un puerto local especifico
+	 	{
+    		client.sin_addr.s_addr = INADDR_ANY;				// Recibe clientes con cualquier direccion IP
+		    client.sin_family = AF_INET;						// IPv4
+		 	client.sin_port = htons(local_port);				// Se indica el puerto por donde se comunica el cliente
+
+		    //Bind
+		    if( bind(sock,(struct sockaddr *)&client , sizeof(client)) < 0)
+		    {
+		        //print the error message
+		        perror("bind failed. Error");
+		        return 1;
+	    	}
+	 	}
+
+	    //Connect to remote server
+	    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+	    {
+	        perror("connect failed. Error");
+	        sleep(5);
+	        close(sock);
+	        continue;
+	    }
+	     
+	    puts("ATM Connected\n");
+	    if(flag)
+	    {
+	    	flag = 0;
+	    	if(send(sock , pp , strlen(pp), MSG_NOSIGNAL) < 0)
+	        {
+	            puts("Send failed");
+	            close(sock);
+	        }
+	        memset(pp, '\0', 2048);
+	        
+	        if(send(sock , sp , strlen(sp), MSG_NOSIGNAL) < 0)
+	        {
+	            puts("Send failed");
+	            close(sock);
+	        }
+	        memset(sp, '\0', 2048);
+	    }
+
+	    //keep communicating with server
+	    while(1)
+	    {
+	       
+	    	/*if (!input_timeout(STDIN_FILENO,300)){			// Se esperan 5 minutos para el envío de data.
+
+		        fgets(message, 2048, stdin);
+	    		fprintf (stderr, "\nNo data sent \n");		// Se informa que no se ha enviado data, pero mantiene activa la conexión
+	    		fprintf(stderr,"Please enter message: ");	// y se solicita que ingrese un mensaje.
+
+	    	}else{*/
+
+	    		printf("Enter message: ");
+		        fgets(message, 2048, stdin);
+
+		        if (message[0] == '\0')						// Cuando se manda como entrada un archivo esto hace que al termniar de leerlo culmine el programa
+		           	return 0;
+		        
+		        //Send some data
+		        if(send(sock , message , strlen(message), MSG_NOSIGNAL) < 0)
+		        {
+		            puts("Send failed");
+		            flag = 1;
+		            strcpy(sp,message);
+		            close(sock);
+		            break;    
+		        }
+		        strcpy(pp,message);
+		        memset(message, '\0', 2048);
+	    	//}
+	        
+	        
+	    }
+	}
+	free(pp);
+	free(sp);
+	free(ip_srv);
+    free(message);
+    close(sock);
     return 0;
 }
- 
-/*
- * Conexión con el cliente
- * */
-void *connection_handler(void *socket_desc)
-{
-    //Get the socket descriptor
-    int sock = *(int*)socket_desc;
-    int read_size;
-    MSG message , client_message;
-     
-    
-     
-    //message = "Type \n";
-    //write(sock , message , strlen(message));
-     
-    //Recibe mensaje del cliente
-    while( (read_size = recv(sock , &client_message , sizeof(client_message) , 0)) > 0 )
-    {
-        //end of string marker
-		//client_message[read_size] = '\0';
-		
-		//Envía el mensaje al cliente
-        write(sock , &client_message , strlen(&client_message));
-		
-		//Limpiar Buffer del mensaje
-		//memset(client_message, 0, 2000);
-    }
-     
-    if(read_size == 0)
-    {
-        puts("Cliente Desconectado");
-        fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
-    }
-         
-    return 0;
-} 
---
+++ svr_s.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <string.h>    
+#include <stdlib.h>    
+#include <sys/socket.h>
+#include <arpa/inet.h> 
+#include <unistd.h>    
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <errno.h>
+
+#include "header.h"
+
+
+int input_timeout (int filedes, unsigned int seconds){      // Se crea una función que recibe el file descriptor del socket y una cantidad 
+                                                            // de tiempo en segundos    
+    fd_set set;                                             
+    struct timeval timeout;
+
+
+    FD_ZERO (&set);                                         // Se inicializa el fd del socket
+    FD_SET (filedes, &set);
+
+    timeout.tv_sec = seconds;                               // Inicializa la estructura de tiempo
+    timeout.tv_usec = 0;
+
+    return (select (FD_SETSIZE,&set, NULL, NULL,&timeout)); // Devuelve 1 si está activo el canal; 0 si no está activo y -1 en caso de error
+}
+
+
+
+int main(int argc , char *argv[])
+{
+    int socket_desc , client_sock , c, port;								// port: puerto por donde escuchara el servidor
+    struct sockaddr_in server , client;
+ 	char *output = (char *) malloc(512*sizeof(char));						// Cadena de caracteres donde se almacenara el nombre del archivo de salida	
+
+ 	// Condicionales que permiten que el comando funcione con la sintaxis deseada
+    if (argc != 5)
+    {
+    	printf("Wrong Sintaxis.\nPlease use: svr_s -l <puerto_svr_s> -b <archivo_bitácora>\n");
+    	exit(-1);
+    }
+    if ((strcmp(argv[1],"-l") == 0) && (strcmp(argv[3],"-b") == 0)) 			// Caso svr_s -l <puerto_svr_s> -b <archivo_bitácora>
+    {
+    	port = atoi(argv[2]);
+    	strcpy(output, argv[4]);
+    }
+    else if((strcmp(argv[1],"-b") == 0) && (strcmp(argv[3],"-l") == 0)) 		// Caso svr_s  -b <archivo_bitácora> -l <puerto_svr_s>
+    {
+    	port = atoi(argv[4]);
+    	strcpy(output, argv[2]);
+    }
+    else
+    {
+    	printf("Wrong Sintaxis.\nPlease use: svr_s -l <puerto_svr_s> -b <archivo_bitácora>\n");
+    	exit(-1);
+    }
+ 
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+        printf("Could not create socket");
+    
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET; 					// IPv4
+    server.sin_addr.s_addr = INADDR_ANY;			// Recibe clientes con cualquier direccion IP
+    server.sin_port = htons( port ); 				// Oye por el puerto indicado por el usuario 
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+
+    puts("The SVR module has been initialized");
+
+    //Listen
+    listen(socket_desc , 3);
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming  ATM connections...");
+    c = sizeof(struct sockaddr_in);
+
+    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    {
+        puts("Connection accepted");
+         
+        pthread_t sniffer_thread;
+
+        // Se inicializan parametros que se llevara el hilo a la funcion donde estaran recibiendo informacion del cliente
+        hilo *h = (hilo*) malloc(sizeof(hilo));
+        h->fd = client_sock;
+        h->msg = (char*) malloc(2048*sizeof(char));
+        h->output = (char*) malloc(512*sizeof(char));
+        strcpy(h->output, output);
+        pthread_mutex_init(&h->mutex, NULL);;
+
+        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) h) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+         
+        pthread_detach(sniffer_thread);
+        puts("ATM Connected\n");
+    }
+     
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    return 0;
+}
+
+
+// Funcion que mantiene la conexion con los clientes, escribiendo los mensajes entrantes en un archivo. 
+void *connection_handler(void *socket_desc)
+{
+    //Get the socket descriptor
+
+    hilo *hi = (hilo*) socket_desc; 					// CAST
+    int read_size;
+	FILE* fp;
+    int optval = 1;
+    socklen_t optlen = sizeof(optval);
+ 
+	if(setsockopt(hi->fd, SOL_SOCKET, SO_KEEPALIVE, &optval, optlen) < 0)
+	{
+	    perror("setsockopt()");
+	    close(hi->fd);
+	    exit(-1);
+	}
+
+    //Receive a message from client
+    while( (read_size = read(hi->fd , hi->msg , 2048)) > 0 )
+    {
+        //Send the message back to client
+        int aux = 0;
+
+        // Verifico si el mensaje entrante posee informacion de interes para generar una alerta
+        aux = alert(hi->msg);
+
+
+        // Comienzo de region critica, solo un hilo pasa a la vez
+		pthread_mutex_lock(&hi->mutex);
+
+
+		if(!(fp = fopen(hi->output,"a")))
+		{
+			printf("Error, Permission denied. Non generated report\n");
+			exit(-1);
+		}   
+	 	if (alert(hi->msg))
+	 	{
+	 		printf("ALERT: %s\n", hi->msg);
+	 	 	fprintf(fp, "ALERT: %s",hi->msg);	
+	 	}
+	 	else
+	 	{
+
+            fprintf(fp, "%s",hi->msg);
+            printf("%s\n", hi->msg);
+            
+            while (!input_timeout(hi->fd,300)){   // Se esperan 5 minutos para el envío de data.
+
+                fprintf (stderr, "\nNo data sent \n");              // Se informa que no se ha enviado data, pero mantiene activa la conexión
+                //fprintf(stderr,"Please enter message ");            // y se solicita que ingrese un mensaje.
+            }
+	 		    
+
+            
+		}
+		fclose(fp);
+  		pthread_mutex_unlock(&hi->mutex); 
+  		// Fin region critica
+
+        memset(hi->msg, '\0', strlen(hi->msg));
+        fflush(stdout);
+    }
+
+
+     
+    if(read_size == 0)
+    {
+        puts("ATM Disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+        perror("recv failed");
+             
+    //Free the socket pointer
+    free(socket_desc);
+    pthread_exit(NULL);
+    return 0;
+}
+
+
+//Funcion que compara cadenas de caracteres y retorna 1 si hay coincidencia (hay alerta) y cero de  lo contrario
+int alert(char *msg)
+{
+
+    int code1 = msg[12] - '0';
+    int code2 = msg[13] - '0';
+
+    if (code1 == 0)
+    {
+        for (int j = 1; j < 10; j++)
+            if(code2 == j)
+                return 1;
+
+    }else if (code1 == 1)
+    {
+        for (int j = 0; j < 3; j++)
+            if(code2 == j)
+                return 1;
+    }
+    return 0;
+}
